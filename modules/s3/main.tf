26d8dbd130e0ea01b3ab2019619559ced02c940d
prvnsinghal1 <pra10654@gmail.com>
2023-12-11 15:17:13 +0530

    revert the change

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -20,31 +20,29 @@ resource "aws_s3_bucket" "s3_bucket" {
 }
 
 resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-  bucket = aws_s3_bucket.s3_bucket.id
+  bucket = aws_s3_bucket.s3_bucket.bucket
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = "${aws_s3_bucket.bucket.id}"
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "${aws_iam_user.user.arn}"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+        ],
+        Resource = [
+          "${aws_s3_bucket.s3_bucket.arn}/*",
+        ],
       },
-      "Action": [ "s3:*" ],
-      "Resource": [
-        "${aws_s3_bucket.bucket.arn}",
-        "${aws_s3_bucket.bucket.arn}/*"
-      ]
-    }
-  ]
-}
-EOF
+    ],
+  })
 }
