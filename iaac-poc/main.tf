79dbe1e798ee4c3704029c12e96f70a32ba43023
prvnsinghal1 <pra10654@gmail.com>
2023-12-11 13:13:05 +0530

    added mandatory tags

--- iaac-poc/main.tf
+++ iaac-poc/main.tf
@@ -3,6 +3,13 @@
 module "s3_bucket_poc" {
   source        = "./../modules/s3"
   environment   = "poc"
+  appName       = "Terraform"
+  infraOwner    = "sre-cloud-reliability@tavisca.com"
+  name          = "IaaC module for Provisioning"
+  businessUnit  = "travel.poc"
+  backup        = "no"
+  dataClassification  = "internal"
+  product       = "poap"
   region        = "us-east-1"
   bucket_name   = "cloud-sre-poc-bucket-demo"
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -2,6 +2,34 @@ variable "environment" {
   description = "The environment name (e.g., poc, qa, stage, prod)"
   type        = string
 }
+variable "appName" {
+  description = "The App name (e.g., Terraform )"
+  type        = string
+}
+variable "infraOwner" {
+  description = "The Infra Owner (e.g., sre-cloud-reliability@tavisca.com )"
+  type        = string
+}
+variable "name" {
+  description = "The name (e.g., IaaC module for Provisioning)"
+  type        = string
+}
+variable "businessUnit" {
+  description = "The environment name (e.g., travel.poc)"
+  type        = string
+}
+variable "backup" {
+  description = "The environment name (e.g., yes or no)"
+  type        = string
+}
+variable "dataClassification" {
+  description = "The environment name (e.g., internal)"
+  type        = string
+}
+variable "product" {
+  description = "The environment name (e.g., poap )"
+  type        = string
+}
 
 variable "region" {
   description = "The AWS region"
